0d4a2eb97d5747cc27b8d357f48369143ec5ca64
Kasumi <kasumi.null@yandex.com>
2021-09-07 15:23:23 -0500

    remove google spyware

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -105,23 +105,6 @@ export default class MyDocument extends Document {
             type="text/javascript"
             src="https://app.intotheblock.com/widget.js"
           />
-                   {/* Global Site Tag (gtag.js) - Google Analytics */}
-                   <script
-            async
-            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
-          />
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${GA_TRACKING_ID}', {
-              page_path: window.location.pathname,
-            });
-          `,
-            }}
-          />
         </Head>
         <body className="no-js">
           <script
