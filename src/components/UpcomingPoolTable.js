395f73adadcfeab30726a77c2d0f42b6a25bd5c8
Kasumi <kasumi.null@yandex.com>
2021-11-12 22:05:05 -0600

    feat: add allocation change & note about algo

--- src/components/UpcomingPoolTable.js
+++ src/components/UpcomingPoolTable.js
@@ -16,81 +16,243 @@ const useStyles = makeStyles((theme) => ({
     width: theme.spacing(3),
     height: theme.spacing(3),
   },
+  lost: {
+    color: theme.palette.negative.main,
+  },
+  gained: {
+    color: theme.palette.positive.main,
+  },
+  brandnew: {
+    color: 'magenta',
+  },
 }));
 
 export default function UpcomingPoolTable({ pairs, ...rest }) {
   const classes = useStyles();
 
+  const currentFarms = {
+    "0x674a71e69fe8d5ccff6fdcf9f1fa4262aa14b154": {
+      allocPoint: 361916384,
+    },
+    "0x437e444365ad9ed788e8f255c908bcead5aea645": {
+      allocPoint: 86288234,
+    },
+    "0x80f712670d268cf2c05e7162674c7466c940ebe3": {
+      farmId: 0,
+      allocPoint: 213339573,
+    },
+    "0x24f011f12ea45afadb1d4245ba15dcab38b43d13": {
+      farmId: 1,
+      allocPoint: 219428219,
+    },
+    "0x4ff52e9d7824ec9b4e0189f11b5aa0f02b459b03": {
+      farmId: 2,
+      allocPoint: 10438301,
+    },
+    "0x1ee39f93450d80981c169e59c8a641a3bc853a09": {
+      farmId: 3,
+      allocPoint: 7452498,
+    },
+    "0xc98552ad7dfc5daabad2660df378e0070ca75efc": {
+      farmId: 4,
+      allocPoint: 11337587,
+    },
+    "0x287a276401cadbe50d5c0398137490e6d45830dd": {
+      farmId: 5,
+      allocPoint: 5359412,
+    },
+    "0x41075d2ea8bef1cafb24d9bd2061b620cbc05b60": {
+      farmId: 6,
+      allocPoint: 5231764,
+    },
+    "0xc47b0b4b51ee06de0daf02517d78f0473b776633": {
+      farmId: 9,
+      allocPoint: 13828619,
+    },
+    "0xd6ecadb40b35d17f739ec27285759d0ca119e3a1": {
+      farmId: 10,
+      allocPoint: 8850010,
+    },
+    "0xfcf26e0eb200692b3002f941eea0486d2e901aa9": {
+      farmId: 11,
+      allocPoint: 5519864,
+    },
+    "0xcfcbc90e617a3996355761b52df2830b7b6718d0": {
+      farmId: 12,
+      allocPoint: 4639249,
+    },
+    "0xf9d33abfaf59fd19077f44399a8971621cd2ea55": {
+      farmId: 13,
+      allocPoint: 3871740,
+    },
+    "0xcabdb1321ceab169935a0c9d4c856250766c3df7": {
+      farmId: 14,
+      allocPoint: 3279854,
+    },
+    "0xbe48dc2353a460668a5d859c66e4472661581998": {
+      farmId: 15,
+      allocPoint: 3150102,
+    },
+    "0x12e03015a85a0c2c1eca69486147608abb0b801c": {
+      farmId: 16,
+      allocPoint: 2859362,
+    },
+    "0x6b68f5d7d0531207a01e9ac16cfcd223d2139d28": {
+      farmId: 17,
+      allocPoint: 2806248,
+    },
+    "0x24615e918ad078900bfe13f4cd26876bae64dd75": {
+      farmId: 18,
+      allocPoint: 2603255,
+    },
+    "0xa331430473aba2337698fd95a7c2fcf376debfb1": {
+      farmId: 19,
+      allocPoint: 2579629,
+    },
+    "0x1c47c2a72e86b9b488f436f7ac76acc61e531926": {
+      farmId: 20,
+      allocPoint: 2551975,
+    },
+    "0xa32b73445dbc075da5054503171362d790164dc9": {
+      farmId: 21,
+      allocPoint: 2539125,
+    },
+    "0xe3e155c22685f7ceab3f429ca60f302bcfb13616": {
+      farmId: 22,
+      allocPoint: 2537287,
+    },
+    "0x0663b29e3caa8f2db0313ea8b3e942a0431429cf": {
+      farmId: 23,
+      allocPoint: 2516289,
+    },
+    "0x211c0d74b1213a40bdfd61490a9893353544ea46": {
+      farmId: 24,
+      allocPoint: 2514742,
+    },
+    "0x8e5edb62775c1cd003804ec2a8242e5e0393876b": {
+      farmId: 25,
+      allocPoint: 2509731,
+    },
+    "0x49260567a5610414954a1d8f0e7774104fc5caed": {
+      farmId: 26,
+      allocPoint: 2503518,
+    },
+    "0x64c379ab93b859ada71b8abaca77bed104a5dbca": {
+      farmId: 27,
+      allocPoint: 2499024,
+    },
+    "0xfec4202e22d0cd950afc52622114e787fffa0f53": {
+      farmId: 28,
+      allocPoint: 2499024,
+    },
+    "0x98a03761fe62b9a1fd7888d86f70e94a40acd511": {
+      farmId: 29,
+      allocPoint: 2499024,
+    },
+  };
+
   const rows = pairs
     .filter((row) => {
       return !PAIR_DENY.includes(row.id);
     })
-    .map((pair) => {
+    .map((pair, idx) => {
       return {
         ...pair,
+        idx,
         displayName: `${pair.token0.symbol.replace(
           "WBCH",
           "BCH"
         )}-${pair.token1.symbol.replace("WBCH", "BCH")}`,
+        allocDelta: currentFarms.hasOwnProperty(pair.id) ? pair.allocation - currentFarms[pair.id].allocPoint : 0,
       };
     });
 
   return (
-    <div className={classes.root}>
-      <SortableTable
-        title="Pools"
-        // orderBy={orderBy}
-        // order={order}
-        columns={[
-          // {
-          //   key: "id",
-          //   label: "#",
-          // },
-          {
-            key: "displayName",
-            label: "Name",
-            render: (row, index) => {
-              return (
-                <Box display="flex" alignItems="center">
-                  <PairIcon
-                    base={row.token0.id}
-                    quote={row.token1.id}
-                  />
-                  <Link href={`/pairs/${row.id}`} variant="body2" noWrap>
-                    {row.displayName}
-                  </Link>
-                </Box>
-              );
+    <>
+      <Box display="flex" alignItems="center">
+        <Typography variant="subtitle2" noWrap>
+          Upcoming farms are selected by a weighting algorithm described here: {' '}
+          <Link href={`https://docs.mistswap.fi/products/amm-exchange/farm-allocation-algorithm/`} variant="body2" noWrap>
+            Farm Allocation Algorithm
+          </Link>
+        </Typography>
+      </Box>
+      <div className={classes.root}>
+        <SortableTable
+          title="Pools"
+          // orderBy={orderBy}
+          // order={order}
+          columns={[
+            // {
+            //   key: "id",
+            //   label: "#",
+            // },
+            {
+              key: "displayName",
+              label: "Name",
+              render: (row, index) => {
+                return (
+                  <Box display="flex" alignItems="center">
+                    <PairIcon
+                      base={row.token0.id}
+                      quote={row.token1.id}
+                    />
+                    <Link href={`/pairs/${row.id}`} variant="body2" noWrap>
+                      {row.displayName}
+                    </Link>
+                  </Box>
+                );
+              },
             },
-          },
-          {
-            key: "allocation",
-            label: "Allocation",
-            render: (row) => (
-              <Typography variant="subtitle2" noWrap>
-                <Percent
-                  percent={Number(row.allocation / 10000000).toFixed(2)}
-                  display="inline"
-                />
-                {' '} / {formatDecimal(row.allocation / 1000000000 * 1568400)} MIST per day
-              </Typography>
-            )
-          },
-          {
-            key: "accVolume",
-            label: "Volume",
-            render: (row) => formatCurrency(row.accVolume),
-          },
-          {
-            key: "volatility",
-            label: "Volatility",
-            render: (row) => formatDecimal(row.volatility),
-          },
-        ]}
-        rows={rows}
-        {...rest}
-      />
-    </div>
+            {
+              key: "allocation",
+              label: "Allocation",
+              render: (row) => (
+                <Typography variant="subtitle2" noWrap>
+                  <Percent
+                    percent={Number(row.allocation / 10000000).toFixed(2)}
+                    display="inline"
+                  />
+                  {' '} / {' '}
+                  {formatDecimal(row.allocation / 1000000000 * 1568400)} MIST per day
+                </Typography>
+              )
+            },
+            {
+              key: "allocDelta",
+              label: "Allocation Change",
+              render: (row) => (
+                <Typography variant="subtitle2" noWrap>
+                  {(row.allocDelta != 0) ? (
+                    <Percent
+                      className={row.allocDelta >= 0 ? classes.gained : classes.lost}
+                      percent={Number(row.allocDelta / 10000000).toFixed(2)}
+                      display="inline"
+                    />
+                  ) : (
+                    <Typography className={classes.brandnew} display="inline">
+                      NEW
+                    </Typography>
+                  )}
+                </Typography>
+              )
+            },
+            {
+              key: "accVolume",
+              label: "Volume",
+              render: (row) => formatCurrency(row.accVolume),
+            },
+            {
+              key: "volatility",
+              label: "Volatility",
+              render: (row) => formatDecimal(row.volatility),
+            },
+          ]}
+          rows={rows}
+          {...rest}
+        />
+      </div>
+    </>
   );
 }
 
