a11fdd089096fc218ee3c3f194553abd68d3689b
Kasumi <kasumi.null@yandex.com>
2021-12-01 14:37:31 -0600

    feat: disable fire

--- src/pages/pools/upcoming.js
+++ src/pages/pools/upcoming.js
@@ -50,6 +50,7 @@ function UpcomingPoolsPage() {
           "0x8803805547b4b7dd1e4d9492a43bf6418447fcb0": "", // LZD
           "0x654adbec36ae3b61255368af2fbaf6302a18fcb5": "", // Akita
           "0xb952907d7b55789209c32353269bab3f9316925b": "", // GEM
+          "0x225fca2a940cd5b18dfb168cd9b7f921c63d7b6e": "", // FIRE
         };
         return ! (blacklist.hasOwnProperty(v.token0.id) || blacklist.hasOwnProperty(v.token1.id));
       })
