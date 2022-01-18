4b06708ef5d843aad647365c4dc20062baff49db
Kasumi <kasumi.null@yandex.com>
2022-01-18 00:23:03 -0600

    feat: complete upgrade

--- src/core/currentFarms.js
+++ src/core/currentFarms.js
@@ -1,27 +1,27 @@
 const farms = {
   "0x674A71E69fe8D5cCff6fdcF9F1Fa4262Aa14b154": {
     farmId: 7,
-    allocPoint: 314847489,
+    allocPoint: 237920395,
   },
   "0x437E444365aD9ed788e8f255c908bceAd5AEA645": {
     farmId: 8,
-    allocPoint: 57663568,
+    allocPoint: 26657186,
   },
   "0x80F712670d268cf2C05e7162674c7466c940eBE3": {
     farmId: 0,
-    allocPoint: 56352041,
+    allocPoint: 25093564,
   },
   "0x24f011f12Ea45AfaDb1D4245bA15dCAB38B43D13": {
     farmId: 1,
-    allocPoint: 209238939,
+    allocPoint: 243043661,
   },
   "0x4fF52e9D7824EC9b4e0189F11B5aA0F02b459b03": {
     farmId: 2,
-    allocPoint: 17931430,
+    allocPoint: 14875963,
   },
   "0x1EE39F93450d80981c169E59C8A641a3bC853A09": {
     farmId: 3,
-    allocPoint: 6753233,
+    allocPoint: 5510936,
   },
   "0xc98552Ad7DFC5daabAd2660DF378e0070ca75Efc": {
     farmId: 4,
@@ -33,27 +33,27 @@ const farms = {
   },
   "0x41075d2Ea8BEF1CAfb24D9Bd2061b620cbc05B60": {
     farmId: 6,
-    allocPoint: 6004967,
+    allocPoint: 6282703,
   },
   "0xc47B0B4B51EE06De0daF02517D78f0473B776633": {
     farmId: 9,
-    allocPoint: 75190888,
+    allocPoint: 77570410,
   },
   "0xD6EcaDB40b35D17f739Ec27285759d0ca119e3A1": {
     farmId: 10,
-    allocPoint: 15083142,
+    allocPoint: 27633378,
   },
   "0xFCf26E0EB200692B3002f941eea0486d2E901aA9": {
     farmId: 11,
-    allocPoint: 5866148,
+    allocPoint: 0,
   },
   "0xCFcBC90e617a3996355761b52dF2830B7b6718d0": {
     farmId: 12,
-    allocPoint: 6496708,
+    allocPoint: 9128453,
   },
   "0xf9D33ABfaF59fd19077f44399A8971621Cd2eA55": {
     farmId: 13,
-    allocPoint: 11069949,
+    allocPoint: 5095820,
   },
   "0xCabdb1321CEAb169935a0c9d4c856250766C3df7": {
     farmId: 14,
@@ -61,11 +61,11 @@ const farms = {
   },
   "0xbE48dC2353a460668A5D859C66e4472661581998": {
     farmId: 15,
-    allocPoint: 6887140,
+    allocPoint: 12964068,
   },
   "0x12E03015A85A0c2c1eca69486147608ABB0b801c": {
     farmId: 16,
-    allocPoint: 5801194,
+    allocPoint: 0,
   },
   "0x6B68f5D7d0531207a01e9AC16cfCd223D2139D28": {
     farmId: 17,
@@ -73,11 +73,11 @@ const farms = {
   },
   "0x24615e918AD078900BfE13F4cd26876Bae64dD75": {
     farmId: 18,
-    allocPoint: 6720317,
+    allocPoint: 11887857,
   },
   "0xa331430473ABA2337698fD95a7c2fCf376DEbFb1": {
     farmId: 19,
-    allocPoint: 5923315,
+    allocPoint: 5275048,
   },
   "0x1c47c2a72e86B9B488f436F7aC76ACc61e531926": {
     farmId: 20,
@@ -93,7 +93,7 @@ const farms = {
   },
   "0x0663B29E3CAa8F2DB0313eA8B3E942a0431429cf": {
     farmId: 23,
-    allocPoint: 6841696,
+    allocPoint: 4818246,
   },
   "0x211c0d74b1213A40Bdfd61490A9893353544ea46": {
     farmId: 24,
@@ -109,7 +109,7 @@ const farms = {
   },
   "0x64c379ab93b859AdA71b8AbACA77BeD104a5DbCa": {
     farmId: 27,
-    allocPoint: 8519902,
+    allocPoint: 30605515,
   },
   "0xFEC4202E22d0cd950aFC52622114e787FFFa0F53": {
     farmId: 28,
@@ -121,51 +121,67 @@ const farms = {
   },
   "0x8221D04A71FcD0Dd3d096cB3B49E22918095933F": {
     farmId: 30,
-    allocPoint: 20997939,
+    allocPoint: 17930897,
   },
   "0x5775D98022590dc60E9c4Ae0a1c56bF1fD8fcaDC": {
     farmId: 31,
-    allocPoint: 36376150,
+    allocPoint: 40393879,
   },
   "0x20943aD7855bdE06Dd41BB89C9D2efE05DB329EC": {
     farmId: 32,
-    allocPoint: 6015684,
+    allocPoint: 0,
   },
   "0xB02A135992478a485D9DD771092CdD8B4487594A": {
     farmId: 33,
-    allocPoint: 14323756,
+    allocPoint: 20467117,
   },
   "0xE75Ec02F28bC0E1ca1794FbFFe8229ac1662075E": {
     farmId: 34,
-    allocPoint: 5804058,
+    allocPoint: 7160695,
   },
   "0x99057a0cB475D1c4d950d552E77e9E68CdDb8261": {
     farmId: 35,
-    allocPoint: 30856981,
+    allocPoint: 32129423,
   },
   "0x1F354956DE4A7Ed71308225De94a27b35A84EA57": {
     farmId: 36,
-    allocPoint: 22679554,
+    allocPoint: 12674312,
   },
   "0xBe8C7C35103c443844Ef234cFFd73a491Df6f503": {
     farmId: 37,
-    allocPoint: 12342954,
+    allocPoint: 28877513,
   },
   "0xB31f44E525Cc07037E55bd448004CfF66f1fa878": {
     farmId: 38,
-    allocPoint: 8893292,
+    allocPoint: 0,
   },
   "0x49F8C72fCA1f6F62411da1Aa451c479e1324Eb8f": {
     farmId: 39,
-    allocPoint: 5992823,
+    allocPoint: 21922370,
   },
   "0xEA5038043364830c489D7fd8F95eFE35eaE6f4Ff": {
     farmId: 40,
-    allocPoint: 6925466,
+    allocPoint: 8511556,
   },
   "0x2eA9369dAEE963CeBc0266AE8b98c3E015C59046": {
-      farmId: 41,
-      allocPoint: 5599258,
+    farmId: 41,
+    allocPoint: 24485367,
+  },
+  "0x800632AFC31225813b06185EA8Be8eD571820a50": {
+    farmId: 42,
+    allocPoint: 14061365,
+  },
+  "0x5e937a1E35e1D931FEbB70E2b061ED38c8E43336": {
+    farmId: 43,
+    allocPoint: 10922329,
+  },
+  "0x7E1B9F1e286160A80ab9B04D228C02583AeF90B5": {
+    farmId: 44,
+    allocPoint: 10271271,
+  },
+  "0x4fd950b3cA45d6F40E5187706D3981ee955E06b4": {
+    farmId: 45,
+    allocPoint: 5828683,
   },
 };
 
