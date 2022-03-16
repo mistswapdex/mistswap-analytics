5f7037302d235c3985e60dd0afb1f3860761cb07
Kasumi <kasumi.null@yandex.com>
2022-03-16 00:11:24 -0500

    feat: timelock update

--- src/core/timelockFarms.js
+++ src/core/timelockFarms.js
@@ -5,119 +5,119 @@ const farms = {
   },
   "0x437E444365aD9ed788e8f255c908bceAd5AEA645": {
     farmId: 8,
-    allocPoint: 18895896,
+    allocPoint: 11829753,
   },
   "0x80F712670d268cf2C05e7162674c7466c940eBE3": {
     farmId: 0,
-    allocPoint: 15532938,
+    allocPoint: 3517927,
   },
   "0x24f011f12Ea45AfaDb1D4245bA15dCAB38B43D13": {
     farmId: 1,
-    allocPoint: 300496954,
+    allocPoint: 326465153,
   },
   "0x4fF52e9D7824EC9b4e0189F11B5aA0F02b459b03": {
     farmId: 2,
-    allocPoint: 10921078,
+    allocPoint: 6457972,
+  },
+  "0x1EE39F93450d80981c169E59C8A641a3bC853A09": {
+    farmId: 3,
+    allocPoint: 2250873,
+  },
+  "0x287a276401caDBe50d5C0398137490E6d45830Dd": {
+    farmId: 5,
+    allocPoint: 3597347,
   },
   "0x41075d2Ea8BEF1CAfb24D9Bd2061b620cbc05B60": {
     farmId: 6,
-    allocPoint: 9144225,
+    allocPoint: 3504844,
   },
   "0xc47B0B4B51EE06De0daF02517D78f0473B776633": {
     farmId: 9,
-    allocPoint: 66660671,
+    allocPoint: 52775754,
   },
   "0xD6EcaDB40b35D17f739Ec27285759d0ca119e3A1": {
     farmId: 10,
-    allocPoint: 9508994,
+    allocPoint: 8409482,
   },
   "0xCabdb1321CEAb169935a0c9d4c856250766C3df7": {
     farmId: 14,
-    allocPoint: 15930397,
-  },
-  "0xbE48dC2353a460668A5D859C66e4472661581998": {
-    farmId: 15,
-    allocPoint: 3895864,
+    allocPoint: 13632865,
   },
   "0x24615e918AD078900BfE13F4cd26876Bae64dD75": {
     farmId: 18,
-    allocPoint: 8496585,
+    allocPoint: 6870412,
+  },
+  "0x0663B29E3CAa8F2DB0313eA8B3E942a0431429cf": {
+    farmId: 23,
+    allocPoint: 2098135,
   },
   "0x64c379ab93b859AdA71b8AbACA77BeD104a5DbCa": {
     farmId: 27,
-    allocPoint: 25590307,
+    allocPoint: 40699427,
   },
   "0x8221D04A71FcD0Dd3d096cB3B49E22918095933F": {
     farmId: 30,
-    allocPoint: 5724322,
+    allocPoint: 4186167,
   },
   "0x5775D98022590dc60E9c4Ae0a1c56bF1fD8fcaDC": {
     farmId: 31,
-    allocPoint: 14925656,
+    allocPoint: 5339500,
   },
-  "0xB02A135992478a485D9DD771092CdD8B4487594A": {
-    farmId: 33,
-    allocPoint: 11304180,
+  "0x20943aD7855bdE06Dd41BB89C9D2efE05DB329EC": {
+    farmId: 32,
+    allocPoint: 33107872,
   },
   "0xE75Ec02F28bC0E1ca1794FbFFe8229ac1662075E": {
     farmId: 34,
-    allocPoint: 7553514,
+    allocPoint: 4381924,
   },
   "0x99057a0cb475d1c4d950d552e77e9e68cddb8261": {
     farmId: 35,
-    allocPoint: 9703179,
+    allocPoint: 5666402,
   },
   "0x1f354956de4a7ed71308225de94a27b35a84ea57": {
     farmId: 36,
-    allocPoint: 10091644,
+    allocPoint: 10638048,
   },
   "0xbe8c7c35103c443844ef234cffd73a491df6f503": {
     farmId: 37,
-    allocPoint: 15401755,
+    allocPoint: 9685079,
   },
   "0x49f8c72fca1f6f62411da1aa451c479e1324eb8f": {
     farmId: 39,
-    allocPoint: 16458210,
+    allocPoint: 24990800,
   },
   "0xea5038043364830c489d7fd8f95efe35eae6f4ff": {
     farmId: 40,
-    allocPoint: 10053255,
+    allocPoint: 2310905,
   },
   "0x2ea9369daee963cebc0266ae8b98c3e015c59046": {
     farmId: 41,
-    allocPoint: 7704190,
-  },
-  "0x800632afc31225813b06185ea8be8ed571820a50": {
-    farmId: 42,
-    allocPoint: 8094038,
+    allocPoint: 21876658,
   },
   "0x7e1b9f1e286160a80ab9b04d228c02583aef90b5": {
     farmId: 44,
-    allocPoint: 9154285,
+    allocPoint: 11926171,
+  },
+  "0x4fd950b3cA45d6F40E5187706D3981ee955E06b4": {
+    farmId: 45,
+    allocPoint: 2227072,
   },
   "0xc20a4f3012ba2df47544d4926b19604fa777fb01": {
     farmId: 46,
-    allocPoint: 33935881,
+    allocPoint: 8915111,
   },
   "0x1d5a7bea34ee984d54af6ff355a1cb54c29eb546": {
     farmId: 47,
-    allocPoint: 16930892,
+    allocPoint: 22196070,
   },
   "0xe1b5bc09427710bc4d886ec49654944110b58134": {
     farmId: 48,
-    allocPoint: 12184556,
-  },
-  "0x380094357328488781a0fb31c271a13db7357c1e": {
-    farmId: 49,
-    allocPoint: 10226248,
-  },
-  "0x8c14d399f3e12b702effd16cf27337637b38c84a": {
-    farmId: 50,
-    allocPoint: 10861373,
+    allocPoint: 22492463,
   },
   "0x0151b25a5acf7f2f31bb2ab4358c9fa894db2cb2": {
     farmId: 51,
-    allocPoint: 4618896,
+    allocPoint: 27949800,
   },
 }
 
